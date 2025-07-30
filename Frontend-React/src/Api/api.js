249c921cb1cf1cc58aa7b1f9c815452e12b5aac2
ROHIT BRAMHANKAR <rohitbramhankar08@gmail.com>
2025-07-30 16:59:28 +0530

    diployed frontend

--- Frontend-React/src/Api/api.js
+++ Frontend-React/src/Api/api.js
@@ -1,9 +1,10 @@
 
 import axios from 'axios';
-const DEPLOYED=''
-const LOCALHOST='http://localhost:5454'
+const DEPLOYED='https://tradingplatform-backend.onrender.com'
+// const LOCALHOST='http://localhost:5454'
 
-export const API_BASE_URL = LOCALHOST
+// export const API_BASE_URL = LOCALHOST
+export const API_BASE_URL = DEPLOYED
 
 const api = axios.create({
   baseURL: API_BASE_URL,
